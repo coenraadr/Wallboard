f68960b1d6a0b25073b1f3ce40e2c39f86d579d6
coenraadr <30891751+coenraadr@users.noreply.github.com>
2022-03-18 21:46:34 +0200

    Update wallboard.sh

--- wallboard.sh
+++ wallboard.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
+sudo apt-get install --no-install-recommends chromium-browser
