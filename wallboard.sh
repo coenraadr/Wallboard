1d39e8936a509754db66359d1a7e1cfaef946f1a
coenraadr <30891751+coenraadr@users.noreply.github.com>
2022-03-18 22:01:37 +0200

    Update wallboard.sh

--- wallboard.sh
+++ wallboard.sh
@@ -3,6 +3,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 sudo apt-get install --no-install-recommends chromium-browser -y
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor' > .bash_profile
+
 echo '
 #!/bin/bash
 xset s noblank
@@ -22,4 +23,5 @@ while true; do
 done
 '
 /etc/xdg/openbox/autostart
+
 sudo reboot
