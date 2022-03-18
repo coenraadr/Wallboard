8a191f591e684f43d6b3dcb1ce35f5d991e873a3
coenraadr <30891751+coenraadr@users.noreply.github.com>
2022-03-18 22:22:35 +0200

    Update wallboard.sh

--- wallboard.sh
+++ wallboard.sh
@@ -3,6 +3,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox xdotool -y
 #Install Chromium Browser
 sudo apt-get install --no-install-recommends chromium-browser -y
+sudo su
 #Create bash_profile
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor' > .bash_profile
 #Append autostart
