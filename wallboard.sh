b24a83129bd67081d7cea07cded2c96a93e76909
coenraadr <30891751+coenraadr@users.noreply.github.com>
2022-03-18 21:57:07 +0200

    Update wallboard.sh

--- wallboard.sh
+++ wallboard.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
-sudo apt-get install --no-install-recommends chromium-browser
+sudo apt-get install --no-install-recommends chromium-browser -y
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor' > .bash_profile
+echo '
+#!/bin/bash
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://pimylifeup.com https://www.adafruit.com &
+
+while true; do
+   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+   sleep 10
+done
+'
+/etc/xdg/openbox/autostart
